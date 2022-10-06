97c4f8211a02e2f4417f42042a21a8202b8a1afc
iesteras <105871265+iesteras@users.noreply.github.com>
2022-10-06 16:01:11 +0200

    Add files via upload

---
+++ project4/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("dev.nttdata.com.project4.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
---
+++ project4/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["dev/nttdata/com/project4/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
