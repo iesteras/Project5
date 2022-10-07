6f37ca9fb0018d6fc37ac64dd4eee980549bd41f
iesteras <105871265+iesteras@users.noreply.github.com>
2022-10-07 14:47:47 +0200

    Update View1.controller.js

--- project4/webapp/test/unit/controller/View1.controller.js
+++ project4/webapp/test/unit/controller/View1.controller.js
@@ -1,16 +1,34 @@
 /*global QUnit*/
 
 sap.ui.define([
-	"devnttdatacom/project4/controller/View1.controller"
-], function (Controller) {
+	"devnttdatacom/project4/controller/View1.controller",
+	"sap/ui/base/ManagedObject",
+	"sap/ui/core/mvc/Controller",	
+	"sap/ui/thirdparty/sinon",
+	"sap/ui/thirdparty/sinon-qunit"
+], function (AppController,ManagedObject,Controller,sinon,sinonqunit) {
 	"use strict";
 
-	QUnit.module("View1 Controller");
+	QUnit.module("View1 Controller",{
+	beforeEach: function() {
+		this.oAppController = new AppController();
+		this.oViewStub = new ManagedObject({});
+		//Obtenemos la vista associada al controlador
+		sinon.stub(Controller.prototype, "getView").returns(this.oViewStub);
+	},
 
-	QUnit.test("I should test the View1 controller", function (assert) {
-		var oAppController = new Controller();
-		oAppController.onInit();
-		assert.ok(oAppController);
-	});
+	afterEach: function() {
+		Controller.prototype.getView.restore();
+		this.oViewStub.destroy();
+	}
+			
+	QUnit.test("Check incremental in controller", function (assert) {
+		//Validamos el incremental dentro del controlador
+		//Obtenemos el controlador
+		var oAppController = new AppController();
+		assert.strictEqual(5, oAppController.contador(4) , "Contador was correct with 3");		
+	});	
+		
+}
 
 });
