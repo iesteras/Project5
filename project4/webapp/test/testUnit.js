ac8b808e6e7535541640c8881702ee945b38920e
iesteras <105871265+iesteras@users.noreply.github.com>
2022-10-07 12:30:26 +0200

    Update testUnit.js

--- project4/webapp/test/testUnit.js
+++ project4/webapp/test/testUnit.js
@@ -1,66 +1,19 @@
-const server = require("../server.js");
+const view1Controller = require("../controller/View1.controller.js");
 
 describe("Opentext Service Unit Test", () => {
-	describe("Decrypt Payload", () => {
-		it("+ Decrypt Payload", (done) => {
-            let privateKey = 'defefr'
-            let payload = 'serfdddss'
-            const decryptValue = server.decryptPayload(privateKey, payload)            
-            if (decryptValue && decryptValue !== ""){
+	describe("Testing method count", () => {
+		it("+ method count", (done) => {
+            let param = 3;
+            
+            const incrementado = view1Controller.contador(param)        
+            if (incrementado === 4){
                 done();                    
             }else{
-                done("Cannot Decrypt Payload");    
+                done("Error count");    
             }  
 		});
 	});
 
-    describe("Create Date", () => {
-		it("+ Create Date", (done) => {
-            const valueGetDate = server.getOtDate()                          
-            if (valueGetDate && valueGetDate.length == 9){
-                console.error(valueGetDate);
-                done();                    
-            }else{
-                done("Cannot create Date");    
-            }
-		});
-	});
-
-    describe("Add Headers", () => {
-		it("+ Add Headers", (done) => {
-            let binding = "";
-            binding.username = "TEST";
-            binding.password = "TESTPASS";
-            const namespace = "opentT";
-            const valueGetHeaders = server.addHeaders(binding, namespace)                          
-            if (valueGetHeaders && valueGetHeaders.headers 
-                    && valueGetHeaders.headers.Authorization
-                    && valueGetHeaders.headers.Authorization != ""){                
-                console.error(valueGetHeaders);
-                done();                    
-            }else{                
-                done("Add Headers");    
-            }
-		});
-	});
-
     
-    describe("Generate Token", () => {
-		it("+ Generate Token", (done) => {
-            const idSession = {"user": "TEST" , "password":"TESTPASS"};
-            const username = "opentT";
-            const sDate = server.getOtDate();
-            let credentialot = {};
-            credentialot.value = "valTES";
-            const secretStoreCred = "values";
-            const valueToken = server.generateToken(idSession, username, sDate, credentialot,secretStoreCred)                          
-            if (valueToken && valueToken  != ""){
-                console.error(valueToken);
-                done();                    
-            }else{
-                done("Generate Token");    
-            }
-		});
-	});    
     
 });
