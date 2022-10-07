5b93cbc2d093406fb732fe0e013a8dafe03865dd
iesteras <105871265+iesteras@users.noreply.github.com>
2022-10-07 13:54:08 +0200

    Update testUnit.js

--- project4/webapp/test/testUnit.js
+++ project4/webapp/test/testUnit.js
@@ -4,8 +4,8 @@ describe("Opentext Service Unit Test", () => {
 	describe("Testing method count", () => {
 		it("+ method count", (done) => {
             let param = 3;
-            
-            const incrementado = view1Controller.contador(param)        
+            let instan = new view1Controller();
+            const incrementado = instan.contador(param)        
             if (incrementado === 4){
                 done();                    
             }else{
